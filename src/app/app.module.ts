e65b3aed6edbc35d6a901bcb0ec3f6de20981bd7
GangulyPriti <mondal.18riya@gmail.com>
2024-04-03 22:24:19 +0530

    day 2/100

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './AssignmentOne/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './AssignmentOne/success-alert/success-alert.component';
+import { UsernameComponent } from './AssignmentTwo/username/username.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    UsernameComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,8 +4,25 @@ import { Component } from '@angular/core';
   // selector: '[app-servers]',attribute selector changes
   // selector: '.app-servers',class selector changes
   selector: 'app-servers',
-  template: ` <app-server></app-server>
-    <app-server></app-server>`,
+  // template: ` <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server is created';
+  serverName = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  ngOnInit() {}
+  onCreatedServer() {
+    this.serverCreationStatus = 'Server is Created Name is ' + this.serverName;
+  }
+  onUpdateServerName(e: Event) {
+    this.serverName = (<HTMLInputElement>e.target).value;
+  }
+}
